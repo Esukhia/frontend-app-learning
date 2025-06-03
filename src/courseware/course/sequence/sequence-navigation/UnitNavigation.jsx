755097e535a179b6a8df0379c4618993d687b776
tenkalden <tenkalden007@gmail.com>
2025-06-03 15:10:57 +0530

    clean-up

--- src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
+++ src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
@@ -9,14 +9,22 @@ import {
 } from '@edx/frontend-platform/i18n';
 import { useSelector } from 'react-redux';
 
+// Course components
 import { GetCourseExitNavigation } from '../../course-exit';
 import SidebarTriggers from '../../sidebar/SidebarTriggers';
 import { Trigger as CourseOutlineTrigger } from '../../sidebar/sidebars/course-outline';
 
+// Local components and utilities
 import UnitNavigationEffortEstimate from './UnitNavigationEffortEstimate';
 import { useSequenceNavigationMetadata } from './hooks';
 import messages from './messages';
 
+/**
+ * UnitNavigation component
+ *
+ * Provides navigation controls for course units, including previous/next buttons
+ * and course outline/sidebar triggers when positioned at the top of the page.
+ */
 const UnitNavigation = ({
   intl,
   sequenceId,
@@ -25,9 +33,12 @@ const UnitNavigation = ({
   onClickNext,
   isAtTop,
 }) => {
+  // Get sequence navigation data (first/last unit status and navigation links)
   const {
     isFirstUnit, isLastUnit, nextLink, previousLink,
   } = useSequenceNavigationMetadata(sequenceId, unitId);
+
+  // Get courseId from Redux store
   const { courseId } = useSelector(state => state.courseware);
 
   // Get arrow direction based on RTL settings
@@ -35,6 +46,10 @@ const UnitNavigation = ({
   const prevArrow = isRtl(locale) ? faChevronRight : faChevronLeft;
   const nextArrow = isRtl(locale) ? faChevronLeft : faChevronRight;
 
+  /**
+   * Renders the previous button with appropriate styling and behavior
+   * Disables the button if user is at the first unit
+   */
   const renderPreviousButton = () => {
     const disabled = isFirstUnit;
     return (
@@ -55,6 +70,11 @@ const UnitNavigation = ({
     );
   };
 
+  /**
+   * Renders the next button with appropriate styling and behavior
+   * Shows exit text if user is at the last unit
+   * Disables the button if user is at the last unit and exit is not active
+   */
   const renderNextButton = () => {
     const { exitActive, exitText } = GetCourseExitNavigation(courseId, intl);
     const buttonText = (isLastUnit && exitText) ? exitText : intl.formatMessage(messages.nextButton);
@@ -79,24 +99,36 @@ const UnitNavigation = ({
     );
   };
 
+  /**
+   * Main render function
+   * Renders different layouts based on whether the navigation is at the top or bottom
+   */
   return (
     <div className={classNames('unit-navigation d-flex align-items-center justify-content-between', { 'top-unit-navigation mb-2.5 w-100 mt-n4.5': isAtTop })}>
+      {/* Top navigation area with course outline and sidebar triggers */}
       {isAtTop && (
-        <div className="d-flex align-items-center ml-auto mr-3"> {/* Container for right-aligned triggers */}
-          <div className="mr-2"> {/* Course Outline Trigger - visible on all screen sizes */}
+        <div className="d-flex align-items-center justify-content-between w-100">
+          {/* Course Outline Trigger on left side */}
+          <div className="mr-2">
             <CourseOutlineTrigger isMobileView />
           </div>
-          <SidebarTriggers className="mx-1" />
+          {/* Container for right-aligned triggers with spacing */}
+          <div className="d-flex align-items-center mr-4">
+            <SidebarTriggers className="mx-1" />
+          </div>
         </div>
       )}
 
+      {/* Navigation buttons area */}
       <div className={classNames('d-flex align-items-center', { 'w-100 justify-content-between': !isAtTop })}>
         {isAtTop ? (
-          <div className="d-flex w-100 flex-wrap">
-            <div className="flex-grow-1 flex-basis-0 min-width-0 pr-2">{renderPreviousButton()}</div>
-            <div className="flex-grow-1 flex-basis-0 min-width-0 pl-2">{renderNextButton()}</div>
+          /* Top navigation buttons aligned to the right */
+          <div className="d-flex ml-auto">
+            <div className="mr-2">{renderPreviousButton()}</div>
+            <div>{renderNextButton()}</div>
           </div>
         ) : (
+          /* Bottom navigation buttons */
           <>
             {renderPreviousButton()}
             {renderNextButton()}
@@ -107,6 +139,9 @@ const UnitNavigation = ({
   );
 };
 
+/**
+ * PropTypes for UnitNavigation component
+ */
 UnitNavigation.propTypes = {
   intl: intlShape.isRequired,
   sequenceId: PropTypes.string.isRequired,
@@ -116,6 +151,9 @@ UnitNavigation.propTypes = {
   isAtTop: PropTypes.bool,
 };
 
+/**
+ * Default props for UnitNavigation component
+ */
 UnitNavigation.defaultProps = {
   unitId: null,
   isAtTop: false,
