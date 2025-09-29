0c4729664c1bfd7105583ea7584bc75024647023
Yogesh Bhagat <yogeshbhagat42code@gmail.com>
2025-09-29 18:21:13 +0530

    add 'start discussion' button in course outline page
    make 'start discussion' button in course outline page responsive for all screen sizes
    
    position discussion button below on mobile, left on desktop

--- src/course-home/outline-tab/widgets/StartOrResumeCourseCard.jsx
+++ src/course-home/outline-tab/widgets/StartOrResumeCourseCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Button, Card } from '@openedx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
+import { Forum } from '@openedx/paragon/icons';
+import { ensureConfig, getConfig } from '@edx/frontend-platform';
 
 import { useSelector } from 'react-redux';
 import { sendTrackingLogEvent } from '@edx/frontend-platform/analytics';
 import messages from '../messages';
 import { useModel } from '../../../generic/model-store';
+
+ensureConfig(['DISCUSSIONS_MFE_BASE_URL']);
 
 const StartOrResumeCourseCard = () => {
   const intl = useIntl();
@@ -29,6 +33,8 @@ const StartOrResumeCourseCard = () => {
     },
   } = useModel('outline', courseId);
 
+  const discussionsUrl = `${getConfig().DISCUSSIONS_MFE_BASE_URL}/${courseId}`;
+
   if (!resumeCourseUrl) {
     return null;
   }
@@ -46,14 +52,24 @@ const StartOrResumeCourseCard = () => {
       <Card.Header
         title={hasVisitedCourse ? intl.formatMessage(messages.resumeBlurb) : intl.formatMessage(messages.startBlurb)}
         actions={(
-          <Button
-            variant="brand"
-            block
-            href={resumeCourseUrl}
-            onClick={() => logResumeCourseClick()}
-          >
-            {hasVisitedCourse ? intl.formatMessage(messages.resume) : intl.formatMessage(messages.start)}
-          </Button>
+          <div className="d-flex flex-column flex-lg-row">
+            <Button
+              variant="brand"
+              href={resumeCourseUrl}
+              onClick={() => logResumeCourseClick()}
+              className="order-1 order-lg-2 mb-2 mb-lg-0"
+            >
+              {hasVisitedCourse ? intl.formatMessage(messages.resume) : intl.formatMessage(messages.start)}
+            </Button>
+            <Button
+              variant="outline-brand"
+              href={discussionsUrl}
+              iconBefore={Forum}
+              className="order-2 order-lg-1 mr-lg-2"
+            >
+              {intl.formatMessage({ id: 'start.discussion', defaultMessage: 'Start Discussion' })}
+            </Button>
+          </div>
         )}
       />
       {/* Footer is needed for internal vertical spacing to work out. If you can remove, be my guest */}
