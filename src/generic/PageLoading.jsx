5bb8280f839793de2745e4a22f2b53eea85cb374
Tenzin Choeying <tenzinchoeying@Tenzins-MacBook-Pro-4.local>
2026-09-03 10:42:42 +0530

    fix: added height 100vh for pageloading for footer to be below screen height

--- src/generic/PageLoading.jsx
+++ src/generic/PageLoading.jsx
@@ -22,7 +22,7 @@ export default class PageLoading extends Component {
         <div
           className="d-flex justify-content-center align-items-center flex-column"
           style={{
-            height: '50vh',
+            height: '100vh',
           }}
         >
           <Spinner animation="border" variant="primary" screenReaderText={this.renderSrMessage()} />
